3a576f857fc4bbc850f0d29bb19115f89a1d6486
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2018-07-28 09:46:26 +0200

    first translation from time to hour display

--- Arduino/Matrix_Demo/Matrix_Demo.ino
+++ Arduino/Matrix_Demo/Matrix_Demo.ino
@@ -1,7 +1,13 @@
 //We always have to include the library
 #include "LedControl.h"
 
+
+#define TRACE_ON 1
+
+#ifdef TRACE_ON
 //#define OUTPUT_TRACE 1
+#define TRACE_CLOCK 1
+#endif
 
 #define LED8X8_DATAIN_PIN 12 
 #define LED8X8_CLK_PIN 11 
@@ -9,17 +15,21 @@
 #define LED8X8_COUNT 1
 #define LED8X8_PANEL_0 0
 
+#define MINUTE_SHIFT_THRESHOLD 30
+
+
 
 LedControl led8x8=LedControl(LED8X8_DATAIN_PIN,LED8X8_CLK_PIN,LED8X8_LOAD_PIN,LED8X8_COUNT);
 
 /* we always wait a bit between updates of the display */
-unsigned long delaytime=300;
+unsigned long delaytime=1000;
 unsigned long clock_hour_bitmap=0xFFFFFFFC;
 byte clock_position=0;
 
 void setup() {
- #ifdef OUTPUT_TRACE 
+ #ifdef TRACE_ON 
  Serial.begin(9600);
+ Serial.println("--------->Start<------------");
  #endif
   
   /*
@@ -31,6 +41,8 @@ void setup() {
   led8x8.setIntensity(LED8X8_PANEL_0,1);
   /* and clear the display */
   led8x8.clearDisplay(LED8X8_PANEL_0);
+
+  input_setup(0, 24*4); /* quater hours */
 }
 
 byte mirroredPattern (byte pattern)
@@ -48,43 +60,95 @@ byte mirroredPattern (byte pattern)
 
 void displayUpdate() {
   byte rowPattern;
-    for(int row=0;row<8;row++) {
+  #ifdef OUTPUT_TRACE 
+        Serial.println("-------");     
+  #endif 
+  for(int row=0;row<8;row++) {
 
-      /* Add elemens of hour bitmap to row pattern */
-      switch(row) {
-        case 0: rowPattern=mirroredPattern(clock_hour_bitmap); break;
-        case 1: rowPattern=((clock_hour_bitmap>>20)&B10000000)|((clock_hour_bitmap>>8)&B00000001); break;
-        case 2: rowPattern=((clock_hour_bitmap>>19)&B10000000)|((clock_hour_bitmap>>9)&B00000001); break;
-        case 3: rowPattern=((clock_hour_bitmap>>18)&B10000000)|((clock_hour_bitmap>>10)&B00000001); break;
-        case 4: rowPattern=((clock_hour_bitmap>>17)&B10000000)|((clock_hour_bitmap>>11)&B00000001); break;
-        case 5: rowPattern=((clock_hour_bitmap>>16)&B10000000)|((clock_hour_bitmap>>12)&B00000001); break;
-        case 6: rowPattern=((clock_hour_bitmap>>15)&B10000000)|((clock_hour_bitmap>>13)&B00000001); break;
-        case 7: rowPattern=clock_hour_bitmap>>14;break;
-        } // switch
-
-
-     
-     led8x8.setRow(LED8X8_PANEL_0,row,rowPattern);   
-      #ifdef OUTPUT_TRACE 
-        Serial.println(rowPattern,BIN);
-      #endif
-    }
-         #ifdef OUTPUT_TRACE 
-        Serial.println("-------");
-        Serial.println(clock_hour_bitmap,BIN);
-         Serial.println("-------");       
-      #endif 
+    /* Add elemens of hour bitmap to row pattern */
+    switch(row) {
+      case 0: rowPattern=mirroredPattern(clock_hour_bitmap); break;
+      case 1: rowPattern=((clock_hour_bitmap>>20)&B10000000)|((clock_hour_bitmap>>8)&B00000001); break;
+      case 2: rowPattern=((clock_hour_bitmap>>19)&B10000000)|((clock_hour_bitmap>>9)&B00000001); break;
+      case 3: rowPattern=((clock_hour_bitmap>>18)&B10000000)|((clock_hour_bitmap>>10)&B00000001); break;
+      case 4: rowPattern=((clock_hour_bitmap>>17)&B10000000)|((clock_hour_bitmap>>11)&B00000001); break;
+      case 5: rowPattern=((clock_hour_bitmap>>16)&B10000000)|((clock_hour_bitmap>>12)&B00000001); break;
+      case 6: rowPattern=((clock_hour_bitmap>>15)&B10000000)|((clock_hour_bitmap>>13)&B00000001); break;
+      case 7: rowPattern=clock_hour_bitmap>>14;break;
+      } // switch
+   
+    led8x8.setRow(LED8X8_PANEL_0,row,rowPattern);   
+    #ifdef OUTPUT_TRACE 
+      Serial.println(rowPattern,BIN);
+    #endif
+  }
+  #ifdef OUTPUT_TRACE 
+    Serial.println("-------");       
+  #endif 
 }
 
+void renderClockHourBitmap(int minute_of_the_day) {
 
+  unsigned int minute_of_the_hour = (minute_of_the_day%60);
+  unsigned int hour = (minute_of_the_day/60)%12; /* will be truncated by integer arithmetic */
+
+  unsigned long master_pattern=0;
+  unsigned long simple_pattern=3;
+
+  #ifdef TRACE_CLOCK 
+        Serial.print(hour);Serial.print(":");Serial.println(minute_of_the_hour);
+  #endif 
+  
+  simple_pattern<<=(2+hour*2); /* this will place hour 0 at bit 2 and 3, 1 at 4+5 and so on */
+                                /* FEDCBA98 76543210 FEDCBA98 76543210 */
+                                /* 00000000 00000000 00000000 00001100  = 0 Hrs */
+                                /* 00000000 00000000 01100000 00000000  = 6 Hrs */
+                                /* 00000011 00000000 00000000 00000000  = 11 Hrs */
+                                /* ____aaaa ddddddcc ccccbbbb bbaaaa__  = simple pattern sides abcd*/
+
+
+
+
+  if(minute_of_the_hour >= MINUTE_SHIFT_THRESHOLD) 
+            simple_pattern<<=1; /* Shift the hole pattern to indicate upcoming hour */
+
+  
+
+  /* Add corner bits (+) to the pattern and overlay 11th to 1st hour */
+  /*  ____aaaa ddddddcc ccccbbbb bbaaaaa_  => */
+  /*  ____dddd dd+ccccc c+bbbbbb +aaaaaa+     */
+  /*  84218421 84218421 84218421 84218421     */
+  
+  master_pattern=   ((simple_pattern & 0x0000003c)<<1)   /* -__aaaa- from 0 hr to 1 */
+                  | ((simple_pattern & 0x0f000000)>>23)  /* -aaaa__- from 11 hr to 0 */       
+                  | ((simple_pattern & 0x00000fc0)<<2)   /* -bbbbbb- from 2 hr to 4 */       
+                  | ((simple_pattern & 0x0003f000)<<3)   /* -cccccc- from 5 hr to 7 */       
+                  | ((simple_pattern & 0x00fc0000)<<4)   /* -dddddd- from 5 hr to 7 */       
+                  | (((simple_pattern>>23)&(simple_pattern>>24)& 0x00000001)) /* left upper corner */       
+                  | (((simple_pattern<<2) &(simple_pattern<<1)&  0x00000080))   /* Right upper corner */       
+                  | (((simple_pattern<<3) &(simple_pattern<<2)&  0x00004000))   /* Right lower corner */       
+                  | (((simple_pattern<<4) &(simple_pattern<<3)&  0x00200000)) ;  /* left lower corner */       
+
+ /* Output in inverse, since our 1 should not be lit up  #TBD move this to output */
+
+ 
+  clock_hour_bitmap=~master_pattern;
+ #ifdef TRACE_CLOCK 
+        Serial.println(0x80000000|simple_pattern,BIN);
+        Serial.println(0x80000000|master_pattern,BIN);
+  #endif    
+};
 
 
 
 void loop() { 
+  static int minutes_of_the_day=0;
+  
+  input_switches_scan_tick();
+  minutes_of_the_day+=30; 
+  if(minutes_of_the_day >= 12*60) minutes_of_the_day=0;
+ // if(minutes_of_the_day > 3*60 && minutes_of_the_day < 10*60) minutes_of_the_day=10*60;
+  renderClockHourBitmap(minutes_of_the_day);
   displayUpdate() ;
-  clock_hour_bitmap=clock_hour_bitmap<<1|0x01;
-  if( clock_hour_bitmap==0xCFFFFFFF) clock_hour_bitmap=0xFFFFFFFC;
-  
-  delay(delaytime);
-  
+  delay(delaytime);  
 }
---
+++ Arduino/Matrix_Demo/input.ino
@@ -0,0 +1,194 @@
+/* Functions to handle all input elements */
+
+// Activate general trace output
+
+//#define TRACE_INPUT 1
+
+/* Port constants */
+
+const byte plug_bus_clock_pin=9;
+const byte plug_bus_storage_pin=8;
+const byte plug_bus_data_pin=7;
+
+
+const byte switch_pin_list[]={5,    // ENCODER A
+                              4,    // ENCODER B
+                              3,    // SELECT ( ENCODER PUSH)
+                               2     // Result 
+                              };   
+                         
+#define INPUT_BITIDX_ENCODER_A 0
+#define INPUT_BITIDX_ENCODER_B 2
+#define INPUT_BITIDX_SELECT 4
+#define INPUT_BITIDX_SNOOZe 6
+/*                                         76543210 */
+#define INPUT_ENCODER_A_MASK              B00000011
+#define INPUT_ENCODER_A_PRESSED_PATTERN   B00000001
+#define INPUT_ENCODER_A_RELEASED_PATTERN  B00000010
+
+#define INPUT_ENCODER_B_MASK              B00001100
+#define INPUT_ENCODER_B_PRESSED_PATTERN   B00000100
+#define INPUT_ENCODER_B_RELEASED_PATTERN  B00001000
+
+#define INPUT_ENCODER_AB_MASK             B00001111
+
+#define INPUT_SELECT_MASK              B00110000
+#define INPUT_SELECT_PRESSED_PATTERN   B00010000
+#define INPUT_SELECT_RELEASED_PATTERN  B00100000
+
+#define INPUT_RESULT_MASK              B11000000
+#define INPUT_RESULT_PRESSED_PATTERN   B01000000
+#define INPUT_RESULT_RELEASED_PATTERN  B10000000
+
+const unsigned long input_debounce_cooldown_interval = 5000; //in microseconds, never check individual state again bevor this time is over
+const unsigned long input_scan_interval = 200; //in microseconds, never scan anything state bevore this time is over, 
+
+/* Variable for reducing cpu usage */
+unsigned long lastScanTs=0;
+
+/* Variables for debounce handling */
+
+#define INPUT_DEBOUNCED_CURRENT_STATE_MASK B01010101
+#define INPUT_DEBOUNCED_PREVIOUS_STATE_MASK B10101010
+
+byte raw_state_previous=0;
+byte debounced_state=0;  // can track up to 4 buttons (current at 6420 and previous at 7531) 
+unsigned long stateChangeTs[sizeof(switch_pin_list)];
+
+
+/* Variables for encoder tracking */
+#define ENCODER_IDLE_POSITION 0
+#define ENCODER_START_WITH_A_PATTERN B00000001
+#define ENCODER_START_WITH_B_PATTERN B00000100
+
+byte encoder_transition_state=0;
+
+int input_encoder_value=0;
+int input_encoder_rangeMin=0;
+int input_encoder_rangeMax=45;
+
+
+
+
+/* ********************************************************************************************************** */
+/*               Interface functions                                                                          */
+
+byte input_selectGotPressed() {
+ return (debounced_state&INPUT_SELECT_MASK)==INPUT_SELECT_PRESSED_PATTERN; ; /* We switched from unpressed to pressed */;
+}
+
+byte input_snoozeGotPressed() {
+ return (debounced_state&INPUT_RESULT_MASK)==INPUT_RESULT_PRESSED_PATTERN; ; /* We switched from unpressed to pressed */;
+}
+
+
+byte input_getEncoderValue(){
+  return input_encoder_value;
+}
+
+void input_setEncoderValue(int newValue) {
+  input_encoder_value=newValue;
+  if(input_encoder_value<input_encoder_rangeMin) input_encoder_value=input_encoder_rangeMin;
+  if(input_encoder_value>input_encoder_rangeMax) input_encoder_value=input_encoder_rangeMax;
+}
+
+/* ********************************************************************************************************** */
+/*               S E T U P                                                                                    */
+
+
+void input_setup(int encoderRangeMin, int encoderRangeMax) {
+  
+  /* Initialize switch pins and debounce timer array */
+  for(byte switchIndex=0;switchIndex<sizeof(switch_pin_list);switchIndex++) {
+       pinMode(switch_pin_list[switchIndex], INPUT_PULLUP);
+       stateChangeTs[switchIndex]=0;  // and initialize timer array
+  }
+  
+
+  /* Initalize the encoder storage */
+  input_encoder_rangeMin=encoderRangeMin;
+  input_encoder_rangeMax=encoderRangeMax;
+  input_encoder_value=encoderRangeMin;
+      
+}
+
+
+
+
+/* ********************************************************************************************************** */
+/* the central scanning function to track the state changes of the buttons and switches                       */
+
+void input_switches_scan_tick() {  /* After every tick, especially the flank events must be checked, because they will be lost in the next tick */
+  byte switchIndex;
+  byte bitIndex;
+  byte rawRead;
+  
+  /* copy remembered debounced state to previous debounced state*/
+  debounced_state=(debounced_state&INPUT_DEBOUNCED_CURRENT_STATE_MASK)<<1
+                 |(debounced_state&INPUT_DEBOUNCED_CURRENT_STATE_MASK);
+  
+  if (micros() - lastScanTs < input_scan_interval) return;  /* return if it is to early to scan again */ 
+  lastScanTs = micros();
+
+  /* Collect raw state an transform it to debounced state */
+  for(switchIndex=0;switchIndex<sizeof(switch_pin_list);switchIndex++) {
+    bitIndex=switchIndex<<1;
+    rawRead=!digitalRead(switch_pin_list[switchIndex]); // Read and reverse bit due to INPUT_PULLUP configuration
+
+    
+    if(bitRead(raw_state_previous,bitIndex)!= rawRead) { // we have a flank
+      bitWrite(raw_state_previous,bitIndex,rawRead); // remember the new raw state
+      stateChangeTs[switchIndex]=micros(); // remember  our time
+    } else {  /* no change in raw state */
+      if(bitRead(debounced_state,bitIndex)!= rawRead && // but a change against debounced state
+         (micros()-stateChangeTs[switchIndex]>input_debounce_cooldown_interval))  // and raw is holding it long enough
+          bitWrite(debounced_state,bitIndex,rawRead); // Change our debounce state
+    }
+  }// For switch index
+
+
+  /* Track encoder transitions transaction */
+ 
+    switch(encoder_transition_state) {
+      case ENCODER_IDLE_POSITION:
+          if((debounced_state&INPUT_ENCODER_AB_MASK) 
+               == ENCODER_START_WITH_A_PATTERN ||
+             ((debounced_state&INPUT_ENCODER_AB_MASK)
+              ==ENCODER_START_WITH_B_PATTERN)) {
+              encoder_transition_state=debounced_state&INPUT_ENCODER_AB_MASK;
+              #ifdef INPUT_TRACE
+                Serial.print("T");
+              #endif 
+          };
+          break;
+    
+      case ENCODER_START_WITH_A_PATTERN:
+            if(bitRead(debounced_state,INPUT_BITIDX_ENCODER_A)==0  // A is back open 
+               && ((debounced_state&INPUT_ENCODER_B_MASK) == INPUT_ENCODER_B_RELEASED_PATTERN)){ // B Pin just got opened
+               if(--input_encoder_value<input_encoder_rangeMin) input_encoder_value=input_encoder_rangeMax; 
+            };
+            break;
+      case ENCODER_START_WITH_B_PATTERN:
+            if(bitRead(debounced_state,INPUT_BITIDX_ENCODER_B)==0  // B is back open 
+               && ((debounced_state&INPUT_ENCODER_A_MASK) == INPUT_ENCODER_A_RELEASED_PATTERN)){ // A Pin just got opened
+                 if(++input_encoder_value>input_encoder_rangeMax) input_encoder_value=input_encoder_rangeMin;
+            };
+            break;
+    };
+            
+    /* Reset encoder  transition state, when all debounced 
+       states of the encoder contacts are low */
+    if((debounced_state&
+       INPUT_ENCODER_AB_MASK&
+       INPUT_DEBOUNCED_CURRENT_STATE_MASK)==0) {
+       #ifdef INPUT_TRACE
+                if(encoder_transition_state) {Serial.print(input_encoder_value); Serial.println("<--Encoder idle");}
+              #endif 
+              encoder_transition_state=ENCODER_IDLE_POSITION;
+
+       }
+
+  
+} // void input_switches_tick()
+
+
