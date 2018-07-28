2f13474fc69fb8666adbd956378994b859d17751
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2018-07-28 13:44:54 +0200

    generation of minute bitmap complete

--- Arduino/Matrix_Demo/Matrix_Demo.ino
+++ Arduino/Matrix_Demo/Matrix_Demo.ino
@@ -23,8 +23,10 @@ LedControl led8x8=LedControl(LED8X8_DATAIN_PIN,LED8X8_CLK_PIN,LED8X8_LOAD_PIN,LE
 
 /* we always wait a bit between updates of the display */
 unsigned long delaytime=1000;
-unsigned long clock_hour_bitmap=0xFFFFFFFC;
-byte clock_position=0;
+
+#define MINUTE_FULL_CIRCLE_THRESHOLD 55
+unsigned long clock_hour_bitmap=0xFFFFFFFF;
+unsigned int clock_minute_bitmap=0xFFFF;
 
 void setup() {
  #ifdef TRACE_ON 
@@ -65,14 +67,29 @@ void displayUpdate() {
   #endif 
   for(int row=0;row<8;row++) {
 
-    /* Add elemens of hour bitmap to row pattern */
+    /* Add elemens of hour and minute bitmap to row pattern */
+    /* map=0  H7  H6  H5  H4  H3  H2  H1  H0 
+     *     1  H27                         H8
+     *     2  H26     M3  M2  M1  M0      H9
+     *     3  H25     M11         M4      H10
+     *     4  H24     M10         M5      H11
+     *     5  H23     M9  M8  M7  M6      H12
+     *     6  H22                         H13
+     *     7  H21 H20 H19 H18 H17 H16 H15 H14
+    */
     switch(row) {
       case 0: rowPattern=mirroredPattern(clock_hour_bitmap); break;
       case 1: rowPattern=((clock_hour_bitmap>>20)&B10000000)|((clock_hour_bitmap>>8)&B00000001); break;
-      case 2: rowPattern=((clock_hour_bitmap>>19)&B10000000)|((clock_hour_bitmap>>9)&B00000001); break;
-      case 3: rowPattern=((clock_hour_bitmap>>18)&B10000000)|((clock_hour_bitmap>>10)&B00000001); break;
-      case 4: rowPattern=((clock_hour_bitmap>>17)&B10000000)|((clock_hour_bitmap>>11)&B00000001); break;
-      case 5: rowPattern=((clock_hour_bitmap>>16)&B10000000)|((clock_hour_bitmap>>12)&B00000001); break;
+      case 2: rowPattern=((clock_hour_bitmap>>19)&B10000000)|((clock_hour_bitmap>>9)&B00000001)
+                          | ((mirroredPattern(clock_minute_bitmap)<<2)&B00111100); break;
+      case 3: rowPattern=((clock_hour_bitmap>>18) & B10000000)|((clock_hour_bitmap>>10)&B00000001)
+                          | ((clock_minute_bitmap>>2 & B00000100))
+                          | ((clock_minute_bitmap>>20 & B00100000)); break;
+      case 4: rowPattern=((clock_hour_bitmap>>17)&B10000000)|((clock_hour_bitmap>>11)&B00000001)
+                          | ((clock_minute_bitmap>>3 & B00000100))
+                          | ((clock_minute_bitmap>>19 & B00100000)); break;
+      case 5: rowPattern=((clock_hour_bitmap>>16)&B10000000)|((clock_hour_bitmap>>12)&B00000001)
+                          | ((mirroredPattern(clock_minute_bitmap)>>12)&B00111100); break;
       case 6: rowPattern=((clock_hour_bitmap>>15)&B10000000)|((clock_hour_bitmap>>13)&B00000001); break;
       case 7: rowPattern=clock_hour_bitmap>>14;break;
       } // switch
@@ -87,27 +104,29 @@ void displayUpdate() {
   #endif 
 }
 
-void renderClockHourBitmap(int minute_of_the_day) {
+void renderClockBitmaps(int minute_of_the_day) {
 
   unsigned int minute_of_the_hour = (minute_of_the_day%60);
   unsigned int hour = (minute_of_the_day/60)%12; /* will be truncated by integer arithmetic */
-
+  unsigned int switchOffBar=0;
   unsigned long master_pattern=0;
   unsigned long simple_pattern=3;
+  const unsigned long switchOffPattern=B01111110;
 
   #ifdef TRACE_CLOCK 
         Serial.print(hour);Serial.print(":");Serial.println(minute_of_the_hour);
   #endif 
-  
+
+  /* ********** *
+   *   Hour
+   * **********
+   */
   simple_pattern<<=(2+hour*2); /* this will place hour 0 at bit 2 and 3, 1 at 4+5 and so on */
                                 /* FEDCBA98 76543210 FEDCBA98 76543210 */
                                 /* 00000000 00000000 00000000 00001100  = 0 Hrs */
                                 /* 00000000 00000000 01100000 00000000  = 6 Hrs */
                                 /* 00000011 00000000 00000000 00000000  = 11 Hrs */
                                 /* ____aaaa ddddddcc ccccbbbb bbaaaa__  = simple pattern sides abcd*/
-
-
-
 
   if(minute_of_the_hour >= MINUTE_SHIFT_THRESHOLD) 
             simple_pattern<<=1; /* Shift the hole pattern to indicate upcoming hour */
@@ -118,7 +137,6 @@ void renderClockHourBitmap(int minute_of_the_day) {
   /*  ____aaaa ddddddcc ccccbbbb bbaaaaa_  => */
   /*  ____dddd dd+ccccc c+bbbbbb +aaaaaa+     */
   /*  84218421 84218421 84218421 84218421     */
-  
   master_pattern=   ((simple_pattern & 0x0000003c)<<1)   /* -__aaaa- from 0 hr to 1 */
                   | ((simple_pattern & 0x0f000000)>>23)  /* -aaaa__- from 11 hr to 0 */       
                   | ((simple_pattern & 0x00000fc0)<<2)   /* -bbbbbb- from 2 hr to 4 */       
@@ -129,26 +147,54 @@ void renderClockHourBitmap(int minute_of_the_day) {
                   | (((simple_pattern<<3) &(simple_pattern<<2)&  0x00004000))   /* Right lower corner */       
                   | (((simple_pattern<<4) &(simple_pattern<<3)&  0x00200000)) ;  /* left lower corner */       
 
- /* Output in inverse, since our 1 should not be lit up  #TBD move this to output */
+
+  switchOffBar=(((hour+1)/3)+2)%4;   /* Determine Side to set big bar */ 
+  master_pattern |= switchOffPattern<<(switchOffBar*7);  
+
+  /* write hour bitmap to final memory (Inverted to light up the zeroes) */
+  clock_hour_bitmap=~master_pattern;
 
  
-  clock_hour_bitmap=~master_pattern;
  #ifdef TRACE_CLOCK 
         Serial.println(0x80000000|simple_pattern,BIN);
-        Serial.println(0x80000000|master_pattern,BIN);
+        Serial.print(0x80000000|master_pattern,BIN);
   #endif    
+
+  
+   /* *************
+    *    Minute
+    * ************  */
+
+                     
+  master_pattern=0x0db6;                                /* _24_ */                                                       /* 8  1 */                                                       /* 4  2 */
+                                                        /* _18_ */
+  if(minute_of_the_hour <MINUTE_FULL_CIRCLE_THRESHOLD) master_pattern>>=(4-(minute_of_the_hour)/15)*3;
+  clock_minute_bitmap=master_pattern;
+
+  #ifdef TRACE_CLOCK 
+        Serial.print("\t");
+        Serial.println(0x80000000|master_pattern,BIN);
+  #endif
+  
 };
 
 
 
 void loop() { 
   static int minutes_of_the_day=0;
-  
-  input_switches_scan_tick();
-  minutes_of_the_day+=30; 
-  if(minutes_of_the_day >= 12*60) minutes_of_the_day=0;
+
+  /* Inputs */
+  //input_switches_scan_tick();
+
+  /* logic*/
+  minutes_of_the_day+=5; 
+  if(minutes_of_the_day >= 12*60) minutes_of_the_day=5;
  // if(minutes_of_the_day > 3*60 && minutes_of_the_day < 10*60) minutes_of_the_day=10*60;
-  renderClockHourBitmap(minutes_of_the_day);
+  renderClockBitmaps(minutes_of_the_day);
+
+  /* output */
   displayUpdate() ;
+
+  /* delay */
   delay(delaytime);  
 }
